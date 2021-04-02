0c6f164addf561293275d00f3306a0946e693851
Er-Rahul-Tiwari <rahultiwari01071996@gmail.com>
2021-04-02 20:56:11 +0530

    app.js include

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const decNum = (val,card__price,card__name,update__name,update__price)=>{
     itemVal.value = 0;
   }else{
     itemVal.value = parseInt(itemVal.value) - 1;
-    update__price.innerHTML = `${parseInt(itemPrice.innerHTML)} - ${23`;
+    update__price.innerHTML = parseInt(itemPrice.innerHTML) - 23;
     console.log("item price : " + update__price.innerHTML);
   }
 }
@@ -27,6 +27,7 @@ const incNum = (val,card__price,card__name,update__name,update__price)=>{
   var update__name = document.querySelector(update__name);
   var update__price = document.querySelector(update__price);
 
+  update__name.innerHTML = card__name.innerHTML;
   // console.log(itemPrice.innerHTML)
 
   if( itemVal.value >= availableItems){
@@ -37,47 +38,3 @@ const incNum = (val,card__price,card__name,update__name,update__price)=>{
     update__price.innerHTML = parseInt(itemPrice.innerHTML) + 23;
   }
 }
-
-// var total = document.querySelector("total");
-// var shipping = document.querySelector("shipping");
-// var discount = document.querySelector("discount");
-// var fee = document.querySelector("fee");
-// var total_cart_amt = document.querySelector("total_cart_amt");;
-
-// const decreaseNumber = (incdec, itemprice) => {
-//   var itemval = document.querySelector(incdec);
-//   var itemprice = document.querySelector(itemprice);
-//   console.log(itemprice.innerHTML);
-//   // console.log(itemval.value);
-//   if (itemval.value <= 0) {
-//     itemval.value = 0;
-//     alert("Negative quantity not allowed");
-//   } else {
-//     itemval.value = parseInt(itemval.value) - 1;
-//     itemval.style.background = "#fff";
-//     itemval.style.color = "#000";
-//     itemprice.innerHTML = parseInt(itemprice.innerHTML) - 15;
-//     total.innerHTML = parseInt(total.innerHTML) - 15;
-//     total_cart_amt.innerHTML =
-//       parseInt(total.innerHTML) +
-//       parseInt(shipping.innerHTML);
-//   }
-// };
-// const increaseNumber = (incdec, itemprice) => {
-//   var itemval = document.querySelector(incdec);
-//   var itemprice = document.querySelector(itemprice);
-//   // console.log(itemval.value);
-//   if (itemval.value >= 5) {
-//     itemval.value = 5;
-//     alert("max 5 allowed");
-//     itemval.style.background = "red";
-//     itemval.style.color = "#fff";
-//   } else {
-//     itemval.value = parseInt(itemval.value) + 1;
-//     itemprice.innerHTML = parseInt(itemprice.innerHTML) + 15;
-//     total.innerHTML = parseInt(total.innerHTML) + 15;
-//     total_cart_amt.innerHTML =
-//       parseInt(total.innerHTML) +
-//       parseInt(shipping.innerHTML);
-//   }
-// };
