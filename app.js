54f6e0c90a41709e5afedc1d6c52968d8d3af268
Er-Rahul-Tiwari <rahultiwari01071996@gmail.com>
2021-04-02 22:38:39 +0530

    --- Auto-Git Commit ---

--- app.js
+++ app.js
@@ -1,40 +1,32 @@
 
-var availableItems = 10;
-
-const decNum = (val,card__price,card__name,update__name,update__price)=>{
-  var itemVal = document.querySelector(val);
-  var itemPrice = document.querySelector(card__price);
   var card__name = document.querySelector(card__name);
   var update__name = document.querySelector(update__name);
-  var update__price = document.querySelector(update__price);
-
- update__name.innerHTML = card__name.innerHTML;
-  console.log(card__name.innerHTML);
-  // console.log(itemPrice.innerHTML);
+  
+  var availableItems = 10;
+  
+  const decNum = (val,card__price,update__price)=>{
+    var itemVal = document.querySelector(val);
+    var itemPrice = document.querySelector(card__price);
+    var update__price = document.querySelector(update__price);
   
   if (itemVal.value <= 0) {
     itemVal.value = 0;
   }else{
     itemVal.value = parseInt(itemVal.value) - 1;
-    update__price.innerHTML = parseInt(itemPrice.innerHTML) - 23;
-    console.log("item price : " + update__price.innerHTML);
+    itemPrice.innerHTML = parseInt(itemPrice.innerHTML) - 23;
   }
 }
-const incNum = (val,card__price,card__name,update__name,update__price)=>{
+const incNum = (val,card__price,update__price)=>{
   var itemVal = document.querySelector(val);
   var itemPrice = document.querySelector(card__price);
-  var card__name = document.querySelector(card__name);
-  var update__name = document.querySelector(update__name);
   var update__price = document.querySelector(update__price);
-
-  update__name.innerHTML = card__name.innerHTML;
-  // console.log(itemPrice.innerHTML)
-
+  
   if( itemVal.value >= availableItems){
     itemVal.value = availableItems;
     alert(`Available items only ${availableItems}`);
   }else{
     itemVal.value = parseInt(itemVal.value) + 1;
-    update__price.innerHTML = parseInt(itemPrice.innerHTML) + 23;
+    itemPrice.innerHTML = parseInt(itemPrice.innerHTML) + 23;
+
   }
 }
