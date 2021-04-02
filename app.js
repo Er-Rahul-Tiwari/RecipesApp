951dc45cb7bd83e36249cf033a85e07f2947e2ee
Er-Rahul-Tiwari <rahultiwari01071996@gmail.com>
2021-04-02 20:51:16 +0530

    --- Auto-Git Commit ---

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 
 var availableItems = 10;
 
-const decNum = (val,card__price)=>{
+const decNum = (val,card__price,card__name,update__name,update__price)=>{
   var itemVal = document.querySelector(val);
   var itemPrice = document.querySelector(card__price);
-  // console.log(itemVal.value);
+  var card__name = document.querySelector(card__name);
+  var update__name = document.querySelector(update__name);
+  var update__price = document.querySelector(update__price);
+
+ update__name.innerHTML = card__name.innerHTML;
+  console.log(card__name.innerHTML);
   // console.log(itemPrice.innerHTML);
   
   if (itemVal.value <= 0) {
     itemVal.value = 0;
   }else{
     itemVal.value = parseInt(itemVal.value) - 1;
-    itemPrice.innerHTML = parseInt(itemPrice.innerHTML) - 23;
-    console.log("item price : " + itemPrice.innerHTML);
+    update__price.innerHTML = `${parseInt(itemPrice.innerHTML)} - ${23`;
+    console.log("item price : " + update__price.innerHTML);
   }
 }
-const incNum = (val,card__price)=>{
+const incNum = (val,card__price,card__name,update__name,update__price)=>{
   var itemVal = document.querySelector(val);
   var itemPrice = document.querySelector(card__price);
+  var card__name = document.querySelector(card__name);
+  var update__name = document.querySelector(update__name);
+  var update__price = document.querySelector(update__price);
 
   // console.log(itemPrice.innerHTML)
 
@@ -26,7 +34,7 @@ const incNum = (val,card__price)=>{
     alert(`Available items only ${availableItems}`);
   }else{
     itemVal.value = parseInt(itemVal.value) + 1;
-    itemPrice.innerHTML = parseInt(itemPrice.innerHTML) + 23;
+    update__price.innerHTML = parseInt(itemPrice.innerHTML) + 23;
   }
 }
 
